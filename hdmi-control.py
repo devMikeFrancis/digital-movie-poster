ac2316d1e4769ffbbbf2f7431743c9cf48f6e13a
Don Jones <newelementdesigns@me.com>
2022-07-30 22:27:48 -0400

    fixed errors

--- hdmi-control.py
+++ hdmi-control.py
@@ -14,7 +14,7 @@ def monitor_off():
   global monitor_is_on
   #subprocess.call('/opt/vc/bin/vcgencmd display_power 0', shell=True)
   subprocess.call('echo standby 0 | cec-client -s -d 1', shell=True)
- monitor_is_on = False
+  monitor_is_on = False
 
 def monitor_on():
   global monitor_is_on
@@ -28,15 +28,15 @@ while True:
   movement = GPIO.input(21)
   if movement:
     if timer:
-      print "cancel timer"
+      print("cancel timer")
       timer.cancel()
       timer = False
     if not monitor_is_on:
-      print "monitor on"
+      print("monitor on")
       monitor_on()
   else:
     if not timer:
-      print "start timer"
+      print("start timer")
       timer = Timer(60*5, monitor_off)
       timer.start()
 
