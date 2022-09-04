1f2d97be329e991ef9b74a8c288877cbb52f3158
Don Jones <newelementdesigns@me.com>
2022-09-04 16:40:21 -0400

    wip install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo -e "\n\nUpdating Apt Packages and upgrading latest patches\n"
 apt update -y && apt upgrade -y
 
 echo -e "\n\nInstalling Apache2 Web server\n"
-apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
+apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert sed -y
 
 echo -e "\n\nInstalling PHP & Requirements\n"
 wget -qO /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
@@ -19,7 +19,7 @@ echo -e "\n\nPermissions for /var/www\n"
 chown -R www-data:www-data /var/www
 #chown -R www-data:www-data /var/www
 #sudo chmod -R 770 /var/www/html/
-echo -e "\n\n Permissions have been set\n"
+echo -e "\n\nPermissions have been set\n"
 
 echo -e "\n\nEnabling Modules\n"
 a2dismod php7.4
@@ -65,8 +65,17 @@ service apache2 restart
 echo -e "\n\nApache finshed\n"
 
 echo -e "\n\nConfiguring MySQL\n"
-# Skipping mysql_secure_installation for this since it won't be public facing
 
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | mysql_secure_installation
+
+    y
+    movieposter
+    movieposter
+    y
+    y
+    y
+    y
+EOF
 #Current password: Enter for none
 #Y - Switch to unix_socket authentication
 #Y - Change root password
@@ -78,12 +87,13 @@ echo -e "\n\nConfiguring MySQL\n"
 #Y - Reload privilege tables now
 
 printf "Creating database ...\n"
-mysql -u root -p$mysqlrootpsw -e "CREATE DATABASE movieposter;"
-printf "Create user $dbuser...\n"
-mysql -u root -p$mysqlrootpsw -e "CREATE USER '$dbuser'@localhost IDENTIFIED BY '$dbpass';"
-printf "Grant $dbuser all privileges on $dbname...\n"
-mysql -u root -p$mysqlrootpsw -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@localhost;"
+mysql -u root -pmovieposter -e "CREATE DATABASE movieposter;"
+printf "Create user ...\n"
+mysql -u root -pmovieposter -e "CREATE USER 'movieposter'@localhost IDENTIFIED BY 'movieposter';"
+printf "Grant movieposter all privileges ...\n"
+mysql -u root -pmovieposter -e "GRANT ALL PRIVILEGES ON *.* TO 'movieposter'@localhost;"
 printf "Database created\n"
+
 echo -e "\n\nRestarting MySQL\n"
 service mysql restart
 
@@ -91,19 +101,19 @@ echo -e "\n\nMySQL finshed\n"
 
 echo -e "\n\nInstalling Git\n"
 apt install git -y
-echo -e "\n\n Git installed\n"
+echo -e "\n\nGit installed\n"
 
 echo -e "\n\nInstalling Composer\n"
 wget -O composer-setup.php https://getcomposer.org/installer
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 rm -rf composer-setup.php
-echo -e "\n\n Composer installed\n"
+echo -e "\n\nComposer installed\n"
 
 echo -e "\n\nInstalling Nodejs and PM2\n"
 curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt-get install -y nodejs
 npm install -g pm2
-echo -e "\n\n Nodejs and PM2 installed\n"
+echo -e "\n\nNodejs and PM2 installed\n"
 
 echo -e "\n\nInstalling and Configuring DMP app\n"
 
@@ -135,22 +145,28 @@ npm run build
 php artisan optimize:clear
 php artisan optimize
 
-echo -e "\n\n DMP app finished\n"
+echo -e "\n\nDMP install finished\n"
 
-#echo -e "\n\n HDMI CEC Control\n"
+echo -e "\n\nInstalling HDMI CEC Control\n"
 
-#sudo apt-get install cec-utils -y
-#sudo crontab -e
-#At the bottom of the crontab add:
-#@reboot python3 /var/www/html/hdmi-control.py
-#Save and exit
-#sudo chmod 777 /dev/vchiq
+apt-get install cec-utils -y
+line="@reboot python3 /var/www/html/hdmi-control.py"
+(crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
+chmod 777 /dev/vchiq
 
-#echo -e "\n\n HDMI CEC control finished\n"
+echo -e "\n\nHDMI CEC control finished\n"
 
-#echo -e "\n\n Kiosk Setup\n"
-#apt install chromium-browser -y
+echo -e "\n\nKiosk Setup\n"
+apt install xdotool unclutter -y
+apt install chromium-browser -y
+#Enable autologin
+#sudo raspi-config
+#https://itnext.io/raspberry-pi-read-only-kiosk-mode-the-complete-tutorial-for-2021-58a860474215
 #https://pimylifeup.com/raspberry-pi-kiosk/
-#echo -e "\n\n Kiosk setup finished\n"
+#sudo nano .bash_profile
+#[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor
+echo -e "\n\nKiosk setup finished\n"
+
+echo -e "\n\nAll done!\n"
 
 exit 0
