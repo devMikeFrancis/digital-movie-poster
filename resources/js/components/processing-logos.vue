1086b441491c44cfda2eeebbce5e732821ffc8bf
Don Jones <newelementdesigns@me.com>
2022-09-18 19:52:10 -0400

    fixed spacing issue

--- resources/js/components/processing-logos.vue
+++ resources/js/components/processing-logos.vue
@@ -360,7 +360,7 @@ export default {
     padding-right: 1vw;
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: space-around;
 
     div {
         flex-grow: 1;
