179e46e66b2f93209679de16977b3028cc52ec40
Don Jones <newelementdesigns@me.com>
2022-10-13 20:49:45 -0400

    updated permissions

--- install.sh
+++ install.sh
@@ -38,8 +38,9 @@ apt-get install mariadb-server mariadb-client -y
 
 echo -e "\n\nPermissions for /var/www"
 chown -R www-data:www-data /var/www
-chown -R $1:www-data /var/www/html
-chmod -R 770 /var/www/html
+#chown -R $1:www-data /var/www/html
+chmod -R 775 /var/www/html
+usermod -a -G www-data $1
 echo -e "\nPermissions have been set\n"
 
 echo -e "\n\nEnabling Modules\n"
@@ -171,8 +172,8 @@ echo "Current user: $1"
 
 systemctl start redis.service
 
-chown -R $1:www-data /var/www/html
-chmod -R 770 /var/www/html
+#chown -R $1:www-data /var/www/html
+#chmod -R 770 /var/www/html
 
 php artisan key:generate
 php artisan storage:link
